8637a811e696426310fbfc7986e3a62989398655
Chia Wei Meng Alexander <chiaweimeng.alexander@gmail.com>
2017-03-23 17:19:45 +0800

    implement onebyte_write

--- cs5250/assignment 3/one.c
+++ cs5250/assignment 3/one.c
@@ -50,7 +50,13 @@ ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos)
 
 ssize_t onebyte_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos)
 {
-	/*please complete the function on your own*/
+	if(*f_pos > 0) {
+		return -ENOSPC;
+	} else {
+		copy_from_user(onebyte_data, buf, 1);
+		*f_pos += 1;
+		return 1;
+	}
 }
 
 static int onebyte_init(void)
