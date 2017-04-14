88bc6790a683e8a25d348ba0169eeace4db618b1
Chia Wei Meng Alexander <chiaweimeng.alexander@gmail.com>
2017-04-14 13:42:21 +0800

    initial commit from assignment 3

---
+++ cs5250/assignment 4/thing.c
@@ -0,0 +1,103 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/slab.h>
+#include <linux/errno.h>
+#include <linux/types.h>
+#include <linux/fs.h>
+#include <linux/proc_fs.h>
+#include <asm/uaccess.h>
+
+#define MAJOR_NUMBER 61
+
+/* forward declaration */
+int open(struct inode *inode, struct file *filep);
+int release(struct inode *inode, struct file *filep);
+ssize_t read(struct file *filep, char *buf, size_t count, loff_t *f_pos);
+ssize_t write(struct file *filep, const char *buf, size_t count, loff_t *f_pos);
+static void exit(void);
+
+/* definition of file_operation structure */
+struct file_operations onebyte_fops = {
+	read: read,
+	write: write,
+	open: open,
+	release: release
+};
+
+char *onebyte_data = NULL;
+
+int open(struct inode *inode, struct file *filep)
+{
+	return 0; // always successful
+}
+
+int release(struct inode *inode, struct file *filep)
+{
+	return 0; // always successful
+}
+
+ssize_t read(struct file *filep, char *buf, size_t count, loff_t *f_pos)
+{
+	if(*f_pos > 0) {
+		return 0;
+	} else {
+		copy_to_user(buf, onebyte_data, 1);
+		*f_pos += 1;
+		return 1;
+	}
+}
+
+ssize_t write(struct file *filep, const char *buf, size_t count, loff_t *f_pos)
+{
+	if(*f_pos > 0) {
+		return -ENOSPC;
+	} else {
+		copy_from_user(onebyte_data, buf, 1);
+		*f_pos += 1;
+		return 1;
+	}
+}
+
+static int init(void)
+{
+	int result;
+	// register the device
+	result = register_chrdev(MAJOR_NUMBER, "4mb", &onebyte_fops);
+	if (result < 0) {
+		return result;
+	}
+
+	// allocate one byte of memory for storage
+	// kmalloc is just like malloc, the second parameter is
+	// the type of memory to be allocated.
+	// To release the memory allocated by kmalloc, use kfree.
+	onebyte_data = kmalloc(sizeof(char), GFP_KERNEL);
+	if (!onebyte_data) {
+		onebyte_exit();
+		// cannot allocate memory
+		// return no memory error, negative signify a failure
+		return -ENOMEM;
+	}
+	// initialize the value to be X
+	*onebyte_data = 'X';
+	printk(KERN_ALERT "This is a 4mb byte device module\n");
+	return 0;
+}
+
+static void exit(void)
+{
+	// if the pointer is pointing to something
+	if (onebyte_data) {
+		// free the memory and assign the pointer to NULL
+		kfree(onebyte_data);
+		onebyte_data = NULL;
+	}
+	// unregister the device
+	unregister_chrdev(MAJOR_NUMBER, "4mb");
+	printk(KERN_ALERT "4mb byte device module is unloaded\n");
+}
+
+MODULE_LICENSE("GPL");
+module_init(init);
+module_exit(exit);
