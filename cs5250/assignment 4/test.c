ade181be5d641e13a06a648b6bc88d27203c976a
Chia Wei Meng Alexander <chiaweimeng.alexander@gmail.com>
2017-04-14 18:24:12 +0800

    implement hello ioctl

--- cs5250/assignment 4/test.c
+++ cs5250/assignment 4/test.c
@@ -1,38 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <sys/ioctl.h>
 int lcd;
-void test() {
+#define SCULL_IOC_MAGIC 'k'
+#define SCULL_HELLO _IO(SCULL_IOC_MAGIC, 1)
+void test()
+{
 	int k, i, sum;
 	char s[3];
 	memset(s, '2', sizeof(s));
 	printf("test begin!\n");
-	k = lseek(lcd, 4, SEEK_CUR);
-	printf("lseek = %d\n", k);
 	k = write(lcd, s, sizeof(s));
 	printf("written = %d\n", k);
-	k = lseek(lcd, 0, SEEK_END);
-	printf("lseek = %d\n", k);
-	k = lseek(lcd, -4, SEEK_END);
-	printf("lseek = %d\n", k);
-	k = lseek(lcd, -4, -1);
-	printf("lseek = %d\n", k);
+	k = ioctl(lcd, SCULL_HELLO);
+	printf("result = %d\n", k);
 }
-void initial(char i) {
-	char s[10];
-	memset(s, i, sizeof(s));
-	write(lcd, s, sizeof(s));
-	char c[20] = "";
-	int k = lseek(lcd, 0, SEEK_SET);
-	printf("lseek = %d\n", k);
-}
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 	lcd = open("/dev/thing", O_RDWR);
 	if (lcd == -1) {
-		printf("unable to open lcd");
+		perror("unable to open lcd");
 		exit(EXIT_FAILURE);
 	}
-	initial('1');
 	test();
 	close(lcd);
 	return 0;
--- cs5250/assignment 4/thing.c
+++ cs5250/assignment 4/thing.c
@@ -8,9 +8,13 @@
 #include <linux/proc_fs.h>
 #include <asm/uaccess.h>
 #include <linux/string.h>
+#include <linux/ioctl.h>
 
 #define MAJOR_NUMBER 61
 #define MAX_BYTES 4000000
+#define SCULL_IOC_MAGIC 'k'
+#define SCULL_IOC_MAXNR 14
+#define SCULL_HELLO	_IO(SCULL_IOC_MAGIC, 1)
 
 /* forward declaration */
 int four_mb_open(struct inode *inode, struct file *filep);
@@ -18,6 +22,7 @@ int four_mb_release(struct inode *inode, struct file *filep);
 ssize_t four_mb_read(struct file *filep, char *buf, size_t count, loff_t *f_pos);
 ssize_t four_mb_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos);
 loff_t four_mb_seek(struct file *filep, loff_t f_pos, int seek_t);
+long ioctl_example(struct file *filp, unsigned int cmd, unsigned long arg);
 static void four_mb_exit(void);
 
 /* definition of file_operation structure */
@@ -26,7 +31,8 @@ struct file_operations four_mb_fops = {
 	write: four_mb_write,
 	open: four_mb_open,
 	release: four_mb_release,
-	llseek: four_mb_seek
+	llseek: four_mb_seek,
+	unlocked_ioctl: ioctl_example
 };
 
 char *four_mb_data = NULL;
@@ -127,6 +133,37 @@ static void four_mb_exit(void)
 	}
 	unregister_chrdev(MAJOR_NUMBER, "4mb");
 	printk(KERN_ALERT "4mb byte device module is unloaded\n");
+}
+
+long ioctl_example(struct file *filp, unsigned int cmd, unsigned long arg)
+{
+	int err = 0, tmp;
+	int retval = 0;
+	/*
+	* extract the type and number bitfields, and don't decode
+	* wrong cmds: return ENOTTY (inappropriate ioctl) before access_ok()
+	*/
+	if (_IOC_TYPE(cmd) != SCULL_IOC_MAGIC) return -ENOTTY;
+	if (_IOC_NR(cmd) > SCULL_IOC_MAXNR) return -ENOTTY;
+	/*
+	* the direction is a bitmask, and VERIFY_WRITE catches R/W
+	* transfers. `Type' is user‐oriented, while
+	* access_ok is kernel‐oriented, so the concept of "read" and
+	* "write" is reversed
+	*/
+	if (_IOC_DIR(cmd) & _IOC_READ)
+		err = !access_ok(VERIFY_WRITE, (void __user *)arg, _IOC_SIZE(cmd));
+	else if (_IOC_DIR(cmd) & _IOC_WRITE)
+		err = !access_ok(VERIFY_READ, (void __user *)arg, _IOC_SIZE(cmd));
+	if (err) return -EFAULT;
+	switch(cmd) {
+		case SCULL_HELLO:
+			printk(KERN_WARNING "hello\n");
+			break;
+		default: /* redundant, as cmd was checked against MAXNR */
+			return -ENOTTY;
+	}
+	return retval;
 }
 
 MODULE_LICENSE("GPL");
